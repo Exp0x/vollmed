b5f0852c5a7dbd8d0c6637e1455a03277f6be136
Eleno <alexlemosm@gmail.com>
2024-08-09 16:18:08 -0300

    add proper HTTP Error Handling

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,43 +1,40 @@
-package med.voll.api.medico;
+package med.voll.api.domain.paciente;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
+import lombok.Setter;
+import med.voll.api.domain.endereco.Endereco;
 
-@Table(name="medicos")
-@Entity(name="medico")
+@Table(name = "pacientes")
+@Entity(name = "paciente")
 @Getter
-@NoArgsConstructor
+@Setter
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
-public class Medico {
+@NoArgsConstructor
+public class Paciente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String telefone;
-    private String crm;
-    @Enumerated(EnumType.STRING)
-    private Especialidade especialidade;
-    @Embedded
+    private String cpf;
     private Endereco endereco;
     private Boolean ativo;
 
-    public Medico(DadosCadastroMedico dados) {
+    public Paciente(DadosCadastroPaciente dados) {
         this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
-        this.crm = dados.crm();
-        this.especialidade = dados.especialidade();
+        this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
     }
 
-    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
         if (dados.nome() != null) {
             this.nome = dados.nome();
         }
