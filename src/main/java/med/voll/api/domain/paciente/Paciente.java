8bd6d069706b2e690367813467a96f45c67c5e93
Eleno <alexlemosm@gmail.com>
2024-08-23 12:50:17 -0300

    fix: correct JPA entity name for Paciente

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 import med.voll.api.domain.endereco.Endereco;
 
 @Table(name = "pacientes")
-@Entity(name = "paciente")
+@Entity(name = "Paciente")
 @Getter
 @Setter
 @AllArgsConstructor
